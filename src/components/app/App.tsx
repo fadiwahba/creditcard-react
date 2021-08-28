17c132df6cd9ee7e69c3204dace2c1125e3b4951
Fady Soliman <fady.soliman@valocity.co.nz>
2021-08-28 22:03:35 +1200

    header and navigation components

---
+++ src/components/app/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.scss';
+import Header from '../header/Header';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h1 className="main_title">Welcome!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,59 @@
+import styles from './Navigation.module.scss';
+import { MdKeyboardBackspace } from 'react-icons/md';
+
+interface IProps {
+  isMenuOpen: boolean;
+  onMenuCloseClick: (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => void;
+}
+
+const Navigation: React.FC<IProps> = ({ isMenuOpen, onMenuCloseClick }) => {
+  const handleMenuBtnClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    onMenuCloseClick(e);
+  };
+
+  return (
+    <nav
+      className={
+        isMenuOpen ? styles.menu + ' ' + styles.menu_show : styles.menu
+      }
+    >
+      <div className="flex_row">
+        <button
+          className={styles.menu_back_icon + ' btn btn_icon'}
+          onClick={(e) => handleMenuBtnClick(e)}
+        >
+          <MdKeyboardBackspace size={40} />
+        </button>
+
+        <div className={styles.title}>Menu</div>
+      </div>
+      <div className={styles.menu_content}>
+        <ul>
+          <li>
+            <a href="/">link item</a>
+          </li>
+          <li>
+            <a href="/">link item</a>
+          </li>
+          <li>
+            <a href="/">link item</a>
+          </li>
+          <li>
+            <a href="/">link item</a>
+          </li>
+          <li>
+            <a href="/">link item</a>
+          </li>
+        </ul>
+        <p>This is menu content</p>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
